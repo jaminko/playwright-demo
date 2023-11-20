a6328feba63881dbeb7fbaef2190482e82d0c954
IvanMocherniuk <jaminko@gmail.com>
2023-11-20 23:59:27 +0200

    Updated pet ID for API test

--- API/HttpClientTests/TestGetEndPoint.cs
+++ API/HttpClientTests/TestGetEndPoint.cs
@@ -8,13 +8,13 @@ namespace PlaywrightDemo.API.HttpClientTests
     public class TestGetEndPoint
     {
         private string expectedStatusCode = "StatusCode: 200";
-        private string getUrl = "https://petstore.swagger.io/v2/pet/9223372036854775807";
+        private string getUrl = "https://petstore.swagger.io/v2/pet/9223372036854745978";
         protected HttpClient client;
 
         [SetUp]
         public void SetUp()
         {
-            // 1. Set up the HTTP client
+            // Set up the HTTP client
             client = new HttpClient();
         }
 
@@ -34,7 +34,7 @@ namespace PlaywrightDemo.API.HttpClientTests
         {
             Uri uri = new Uri(getUrl);
 
-            // 2. Creeate request and execute it
+            // Creeate request and execute it
             Task<HttpResponseMessage> responce = client.GetAsync(uri);
             HttpResponseMessage fullMsg = responce.Result;
             var msgText = fullMsg.ToString();
@@ -151,7 +151,33 @@ namespace PlaywrightDemo.API.HttpClientTests
             Console.WriteLine(data);
         }
 
+        [Test]
+        public void GetEndPointUsingSendAsync()
+        {
+            HttpRequestMessage httpRequest = new HttpRequestMessage();
+            httpRequest.RequestUri = new Uri(getUrl);
+            httpRequest.Method = HttpMethod.Get;
+            httpRequest.Headers.Add("Accept", "application/json");
 
+            Task<HttpResponseMessage> responce = client.SendAsync(httpRequest);
+            HttpResponseMessage fullMsg = responce.Result;
+            var msgText = fullMsg.ToString();
+
+            Assert.IsTrue(msgText.Contains(expectedStatusCode), $"Expected '{expectedStatusCode}' could not be found.");
+            Console.WriteLine(msgText);
+
+            // Status code
+            var statusCode = fullMsg.StatusCode;
+            Assert.AreEqual(200, (int)statusCode, "200 status code could not be found.");
+
+            // Responce data
+            var responceContent = fullMsg.Content;
+            Task<string> responceData = responceContent.ReadAsStringAsync();
+            var data = responceData.Result;
+
+            Console.WriteLine();
+            Console.WriteLine(data);
+        }
 
         [TearDown]
         public void TearDown()
