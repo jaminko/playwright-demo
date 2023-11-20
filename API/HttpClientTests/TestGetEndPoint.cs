ceb5052dbc5a97ecff4796dd7ff897bfaaa81c99
IvanMocherniuk <jaminko@gmail.com>
2023-11-20 10:34:06 +0200

    Added new API tests

--- API/HttpClientTests/TestGetEndPoint.cs
+++ API/HttpClientTests/TestGetEndPoint.cs
@@ -77,7 +77,81 @@ namespace PlaywrightDemo.API.HttpClientTests
         public void GetAllEndPointInJsonFormat()
         {
             var requestHeaders = client.DefaultRequestHeaders;
+            requestHeaders.Add("Accept", "application/json");
+
+            Task<HttpResponseMessage> responce = client.GetAsync(getUrl);
+            HttpResponseMessage fullMsg = responce.Result;
+            var msgText = fullMsg.ToString();
+
+            Assert.IsTrue(msgText.Contains(expectedStatusCode), $"Expected '{expectedStatusCode}' could not be found.");
+            Console.WriteLine(msgText);
+
+            // Status code
+            var statusCode = fullMsg.StatusCode;
+            Assert.AreEqual(200, (int)statusCode, "200 status code could not be found.");
+
+            // Responce data
+            var responceContent = fullMsg.Content;
+            Task<string> responceData = responceContent.ReadAsStringAsync();
+            var data = responceData.Result;
+
+            Console.WriteLine();
+            Console.WriteLine(data);
         }
+
+        [Test]
+        public void GetAllEndPointInXmlFormat()
+        {
+            var requestHeaders = client.DefaultRequestHeaders;
+            requestHeaders.Add("Accept", "application/xml");
+
+            Task<HttpResponseMessage> responce = client.GetAsync(getUrl);
+            HttpResponseMessage fullMsg = responce.Result;
+            var msgText = fullMsg.ToString();
+
+            Assert.IsTrue(msgText.Contains(expectedStatusCode), $"Expected '{expectedStatusCode}' could not be found.");
+            Console.WriteLine(msgText);
+
+            // Status code
+            var statusCode = fullMsg.StatusCode;
+            Assert.AreEqual(200, (int)statusCode, "200 status code could not be found.");
+
+            // Responce data
+            var responceContent = fullMsg.Content;
+            Task<string> responceData = responceContent.ReadAsStringAsync();
+            var data = responceData.Result;
+
+            Console.WriteLine();
+            Console.WriteLine(data);
+        }
+
+        [Test]
+        public void GetAllEndPointInJsonFormatUsingAcceptHeader()
+        {
+            var requestHeaders = client.DefaultRequestHeaders;
+            requestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            Task<HttpResponseMessage> responce = client.GetAsync(getUrl);
+            HttpResponseMessage fullMsg = responce.Result;
+            var msgText = fullMsg.ToString();
+
+            Assert.IsTrue(msgText.Contains(expectedStatusCode), $"Expected '{expectedStatusCode}' could not be found.");
+            Console.WriteLine(msgText);
+
+            // Status code
+            var statusCode = fullMsg.StatusCode;
+            Assert.AreEqual(200, (int)statusCode, "200 status code could not be found.");
+
+            // Responce data
+            var responceContent = fullMsg.Content;
+            Task<string> responceData = responceContent.ReadAsStringAsync();
+            var data = responceData.Result;
+
+            Console.WriteLine();
+            Console.WriteLine(data);
+        }
+
+
 
         [TearDown]
         public void TearDown()
