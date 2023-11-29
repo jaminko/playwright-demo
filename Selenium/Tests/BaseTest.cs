2ba47d042f25775c391fe27f65043b0dd2d33002
IvanMocherniuk <jaminko@gmail.com>
2023-11-29 16:29:50 +0200

    Enabled headless mode for github actions

--- Selenium/Tests/BaseTest.cs
+++ Selenium/Tests/BaseTest.cs
@@ -22,7 +22,7 @@ namespace DemoTestFramework.Selenium.Tests
             };
             options.AddArgument("--silent");
             options.AddArgument("log-level=3");
-            //options.AddArgument("--headless");
+            options.AddArgument("--headless");
 
             driver = new ChromeDriver(chromeDriverService, options);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
